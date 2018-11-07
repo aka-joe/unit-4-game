6b3ab4af2c8987784bfa179a620a3396e01d55fd
Joseph Ahn <joseph@ahnga.net>
2018-11-07 14:15:34 -0800

    Added jQuery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,6 @@
-$(document).ready(function () {
+// $(document).ready(function () {
 
+    // Initialize characters
     var character = {
         id: ["rey", "luke", "vader", "kylo"],
         name: ["Rey", "Luke Skywalker", "Darth Vader", "Kylo Ren"],
@@ -8,9 +9,24 @@ $(document).ready(function () {
         ctak: [100, 100, 100, 100]
     }
 
+    // Display characters
     var mainScreen = $(".container");
     for (var i=0; i<character.id.length; i++) {
-        var char = $(".character");
-        var 
+        var char = $("<div class='character'>");
+        var pic = $("<img>").attr({
+            alt: character.name[i],
+            src: "./assets/images/" + character.id[i] + ".jpg",
+            class: "charPic"
+        });
+        var nameBox = $("<div class='overlay'>").attr("id", character.id[i]);
+        var nameText = $("<div class='charName'>").text(character.name[i]);
+
+        $(nameBox).append(nameText);
+
+        $(char).append(pic);
+        $(char).append(nameBox);
+
+        $(mainScreen).append(char);
     }
-}
+    
+// }
