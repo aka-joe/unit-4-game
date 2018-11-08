808cfad2a8d413a6c6d450e27c9a65e66011b269
Joseph <joseph@ahnga.net>
2018-11-07 22:16:01 -0800

    Added selecting attacker UI

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,18 +9,25 @@
         ctak: [100, 100, 100, 100]
     }
 
+    var mainChar = "";
+    var enemyList = ["", "", ""];
+
+    var winHeight = ($(window).height()-450)/2 + $(window).scrollTop();
+    var winWidth = ($(window).width()-300)/2 + $(window).scrollLeft();
+
     // Display characters
     var mainScreen = $(".container");
 
     for (var i=0; i<character.id.length; i++) {
         // Put the element at the center of screen
         var char = $("<div class='character'>").attr("id", character.id[i]).css({
-            "top" : Math.max(0, (($(window).height()-450)/2) + $(window).scrollTop()) +"px",
-            "left" : Math.max(0, (($(window).width()-300)/2) + $(window).scrollLeft()+i*300-450)  +"px"
+            top: winHeight,
+            left: (winWidth + i * 300 - 450)
         });
 
         var pic = $("<img>").attr({
             alt: character.name[i],
+            id: character.id[i]+"Pic",
             src: "./assets/images/" + character.id[i] + ".jpg",
             class: "charPic"
         });
@@ -35,8 +42,25 @@
         $(mainScreen).append(char);
     }
 
-    $(".character").on("click", function() {
-        $(this).animate({'width': '150px'},'slow');
+    // Select main character
+    $(".charPic").on("click", function() {
+        mainChar = $(this).attr("id");
+        selectedMainChar(mainChar.substring(0, mainChar.length - 3));
       });
+
+    // After selected main character
+    function selectedMainChar(m) {
+        var y = 0;
+        for (var i=0; i<4; i++) {
+            if (m === character.id[i]) {
+                $("#" + m + "Name").css("height", "50px");
+                $("#" + m + "Pic").css({"-webkit-filter" : "grayscale(0)", filter: "grayscale(0)"});
+                $("#" + m).animate({"left" : (winWidth - 450)},800);  
+            } else {
+                $("#" + character.id[i]).animate({width: 200, top: (winHeight+75), left: (winWidth + y++ * 200 + 100)});
+            }
+        }
+
+    };
 
 // }
