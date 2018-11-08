a5e87e3fc2e85d91a345c9efb98eea0923fbf98d
Joseph <joseph@ahnga.net>
2018-11-08 01:10:15 -0800

    Added some text into game like attacker, defender and defeated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,17 +5,20 @@ var character = {
     id: ["rey", "luke", "vader", "kylo"],
     name: ["Rey", "Luke Skywalker", "Darth Vader", "Kylo Ren"],
     hp: [100, 120, 140, 160],
-    atk: [160, 140, 120, 100],
-    ctak: [100, 100, 100, 100]
+    atk: [22, 18, 14, 10],
+    ct: [15, 20, 25, 30]
 }
 
 var mainChar = "";
 var enemyList = ["", "", ""];
+var attacker = 0;
+var defender = 0;
 
 var winHeight = ($(window).height() - 450) / 2 + $(window).scrollTop();
 var winWidth = ($(window).width() - 300) / 2 + $(window).scrollLeft();
 
 var gameStatus = "ready";
+var aniSpeed = 600;
 
 // Display characters
 var mainScreen = $(".container");
@@ -33,8 +36,8 @@ for (var i = 0; i < character.id.length; i++) {
         src: "./assets/images/" + character.id[i] + ".jpg",
         class: "charPic"
     });
-    var nameBox = $("<div class='overlay'>").attr("id", character.id[i] + "Name");
-    var nameText = $("<div class='charName'>").text(character.name[i]);
+    var nameBox = $("<div class='overlay'>").attr("id", character.id[i] + "Tag");
+    var nameText = $("<div class='charName'>").text(character.name[i]).attr("id", character.id[i] + "Name");
 
     $(nameBox).append(nameText);
 
@@ -49,7 +52,6 @@ $(".charPic").on("click", function () {
     if (gameStatus === "ready") {
         var selection = $(this).attr("id");
         selection = selection.substring(0, selection.length - 3)
-
         if (mainChar === "") {
             mainChar = selection;
             selectedMainChar(selection);
@@ -68,11 +70,13 @@ function selectedMainChar(m) {
     var x = 0;
     for (var i = 0; i < 4; i++) {
         if (m === character.id[i]) {
-            $("#" + m + "Name").css("height", "50px");
+            attacker = i;
+            $("#" + m + "Tag").css("height", "50px");
             $("#" + m + "Pic").css({ "-webkit-filter": "grayscale(0)", filter: "grayscale(0)" });
-            $("#" + m).animate({ left: (winWidth - 300) }, 800);
+            $("#" + m).animate({ left: (winWidth - 300) }, aniSpeed);
+            $("#" + m + "Name").text("Attacker");
         } else {
-            $("#" + character.id[i]).animate({ width: 200, top: (winHeight + 75), left: (winWidth + x++ * 200 + 100) });
+            $("#" + character.id[i]).animate({ width: 200, top: (winHeight + 75), left: (winWidth + x++ * 200 + 100) }, aniSpeed);
         }
     }
 };
@@ -82,17 +86,27 @@ function firstEnemy(e1) {
     var y = 0;
     for (var i = 0; i < 4; i++) {
         if (e1 === character.id[i]) {
-            $("#" + e1 + "Name").css("height", "50px");
+            defender = i;
+            $("#" + e1 + "Tag").css("height", "50px");
             $("#" + e1 + "Pic").css({ "-webkit-filter": "grayscale(0)", filter: "grayscale(0)" });
-            $("#" + e1).animate({ width: 300, top: winHeight, left: (winWidth + 250) }, 800);
+            $("#" + e1).animate({ width: 300, top: winHeight, left: (winWidth + 250) }, aniSpeed);
+            $("#" + e1 + "Name").text("Defender");
         } else if (mainChar != character.id[i]) {
-            $("#" + character.id[i]).animate({ width: 150, top: (winHeight + y++ * 225), left: (winWidth + 550) });
+            $("#" + character.id[i]).animate({ width: 150, top: (winHeight + y++ * 225), left: (winWidth + 550) }, aniSpeed);
         }
     }
 };
 
 // Selected 2nd enemy
 function secondEnemy(e2) {
+    var e1 = enemyList[0];
+    
+    defender = $.inArray(e2, character.id);
+    $("#" + e2 + "Tag").css("height", "50px");
+    $("#" + e2 + "Pic").css({ "-webkit-filter": "grayscale(0)", filter: "grayscale(0)" });
+    $("#" + e2).animate({ width: 300, top: winHeight, left: (winWidth + 250) }, aniSpeed);
+    $("#" + e2 + "Name").text("Defender");
+
     // Find last enemy
     for (var i = 0; i < 4; i++) {
         var e3 = character.id[i];
@@ -101,14 +115,43 @@ function secondEnemy(e2) {
             i = 4;
         }
     }
-    $("#" + e2 + "Name").css("height", "50px");
-    $("#" + e2 + "Pic").css({ "-webkit-filter": "grayscale(0)", filter: "grayscale(0)" });
-    $("#" + e2).animate({ width: 300, top: winHeight, left: (winWidth + 250) }, 800);
+    $("#" + e3).animate({ width: 150, top: (winHeight), left: (winWidth + 550) }, aniSpeed);
+    $("#" + e1).animate({ width: 150, top: (winHeight + 225), left: (winWidth + 550) }, aniSpeed);
 
-    $("#" + e3).animate({ width: 150, top: (winHeight), left: (winWidth + 550) });
-    $("#" + enemyList[0]).animate({ width: 150, top: (winHeight + 225), left: (winWidth + 550) });
-
-    gameStatus = "fight";
+    defeated(e1); // test
+    thirdEnemy(); // test
 };
+
+// 3rd enemy
+function thirdEnemy() {
+    var e2 = enemyList[1];
+    var e3 = enemyList[2];
+
+    $("#" + e3 + "Tag").css("height", "50px");
+    $("#" + e3 + "Pic").css({ "-webkit-filter": "grayscale(0)", filter: "grayscale(0)" });
+    $("#" + e3).animate({ width: 300, top: winHeight, left: (winWidth + 250) }, aniSpeed);
+    $("#" + e3 + "Name").text("Defender");
+
+    $("#" + e2).animate({ width: 150, top: (winHeight), left: (winWidth + 550) }, aniSpeed);
+    defeated(e2);
+
+    fighting();
+};
+
+// Defeated
+function defeated(d) {
+    $("#" + d + "Pic").css({
+        "-webkit-filter": "grayscale(100%) brightness(40%) blur(2px)",
+        filter: "grayscale(100%) brightness(40%) blur(2px)"
+    });
+    $("#" + d + "Tag").css({height: "100%", color: "black", "background-color": "rgba(255, 255, 255, 0.3)"});
+    $("#" + d + "Name").text("Defeated");
+}
+
+// Fighting mode
+function fighting() {
+    gameStatus = "fight";
+
+}
 
 // }
