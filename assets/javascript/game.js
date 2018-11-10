20103c03716e9b544a4e2eaba8435772ef664e3f
Joseph <joseph@ahnga.net>
2018-11-10 01:04:23 -0800

    Added battle calculation function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,9 +3,9 @@ $(document).ready(function () {
     // Initialize characters
     var character = {
         id: ["Cloud", "Squall", "Lightning", "Noctis"],
-        hp: [100, 120, 140, 160],
-        atk: [22, 18, 14, 10],
-        ct: [15, 20, 25, 30],
+        hp: [140, 160, 180, 200],
+        atk: [31, 24, 17, 10],
+        ct: [15, 25, 35, 45],
         clr: ["rgba(63, 168, 230, 0.7)", "rgba(17, 153, 46, 0.6)", "rgba(199, 60, 136, 0.7)", "rgba(56, 56, 56, 0.7)"]
     }
 
@@ -17,6 +17,7 @@ $(document).ready(function () {
     var winHeight = ($(window).height() - 450) / 2 + $(window).scrollTop();
     var winWidth = ($(window).width() - 300) / 2 + $(window).scrollLeft();
     var aniSpeed = 600;
+    var clicked = false;
 
     // Game status
     // 0 - Waiting to start the game
@@ -24,9 +25,10 @@ $(document).ready(function () {
     // 2 - 1st fighting
     // 3 - Selecting 2nd opponent
     // 4 - 2nd fighting
-    // 5 - Last fighting
-    // 6 - User lose
+    // 5 - Auto selecting last opponent
+    // 6 - Last fighting
     // 7 - User win
+    // 8 - User lose
     var gameStatus = 0;
 
     // Display logo and footer
@@ -41,7 +43,8 @@ $(document).ready(function () {
         // Put the element at the center of screen
         var char = $("<div class='character'>").attr("id", character.id[i]).css({
             top: winHeight,
-            left: (winWidth + i * 300 - 450)
+            left: (winWidth + i * 300 - 450),
+            cursor: "pointer"
         });
 
         var pic = $("<img>").attr({
@@ -51,7 +54,7 @@ $(document).ready(function () {
             class: "charPic",
             hp: character.hp[i],
             atk: character.atk[i],
-            ct: character.ct[i]
+            ct: character.ct[i],
         });
 
         var nameBox = $("<div class='overlay'>").attr("id", character.id[i] + "Tag").css("background-color", character.clr[i]);
@@ -97,7 +100,7 @@ $(document).ready(function () {
             $("#ff").animate({ top: 80, left: 100, width: 350, height: 67 }, aniSpeed);
             $("footer").animate({ right: 50 }, aniSpeed);
             message.css({ "font-size": 40, top: winHeight + 500, left: winWidth - 100 });
-            message.text("SELECT YOUR CHARACTER");
+            message.text("SELECT YOUR CHARACTER").css("cursor", "default");
             message.fadeIn(aniSpeed * 3);
             for (var i = 0; i < character.id.length; i++) {
                 $("#" + character.id[i]).delay(i * 300).fadeIn(aniSpeed);
@@ -131,10 +134,11 @@ $(document).ready(function () {
         for (var i = 0; i < 4; i++) {
             if (m === character.id[i]) {
                 attacker = i;
+                base_atk = character.atk[i];
                 $("#attack").css("background-color", character.clr[i]);
                 $("#" + m + "Tag").css("height", "50px");
                 $("#" + m + "Pic").css({ "-webkit-filter": "grayscale(0)", filter: "grayscale(0)" });
-                $("#" + m).delay(0).animate({ left: (winWidth - 300) }, aniSpeed);
+                $("#" + m).css("cursor", "default").delay(0).animate({ left: (winWidth - 300) }, aniSpeed);
                 $("#" + m + "Name").text("Attacker");
             } else {
                 $("#" + character.id[i]).delay(0).animate({ width: 200, top: (winHeight + 30), left: (winWidth + x++ * 200 + 100) }, aniSpeed);
@@ -153,7 +157,7 @@ $(document).ready(function () {
                 defender = i;
                 $("#" + e1 + "Tag").css("height", "50px");
                 $("#" + e1 + "Pic").css({ "-webkit-filter": "grayscale(0)", filter: "grayscale(0)" });
-                $("#" + e1).delay(0).animate({ width: 300, top: winHeight, left: (winWidth + 250) }, aniSpeed);
+                $("#" + e1).css("cursor", "default").delay(0).animate({ width: 300, top: winHeight, left: (winWidth + 250) }, aniSpeed);
                 $("#" + e1 + "Name").text("Defender");
             } else if (mainChar != character.id[i]) {
                 $("#" + character.id[i]).delay(0).animate({ width: 150, top: (winHeight + y++ * 225), left: (winWidth + 550) }, aniSpeed);
@@ -170,7 +174,7 @@ $(document).ready(function () {
         defender = $.inArray(e2, character.id);
         $("#" + e2 + "Tag").css("height", "50px");
         $("#" + e2 + "Pic").css({ "-webkit-filter": "grayscale(0)", filter: "grayscale(0)" });
-        $("#" + e2).delay(0).animate({ width: 300, top: winHeight, left: (winWidth + 250) }, aniSpeed);
+        $("#" + e2).css("cursor", "default").delay(0).animate({ width: 300, top: winHeight, left: (winWidth + 250) }, aniSpeed);
         $("#" + e2 + "Name").text("Defender");
 
         // Find last enemy
@@ -181,7 +185,7 @@ $(document).ready(function () {
                 i = 4;
             }
         }
-        $("#" + e3).delay(0).animate({ width: 150, top: (winHeight), left: (winWidth + 550) }, aniSpeed);
+        $("#" + e3).css("cursor", "default").delay(0).animate({ width: 150, top: (winHeight), left: (winWidth + 550) }, aniSpeed);
         $("#" + e1).delay(0).animate({ width: 150, top: (winHeight + 225), left: (winWidth + 550) }, aniSpeed);
 
         fighting();
@@ -212,13 +216,13 @@ $(document).ready(function () {
             "-webkit-filter": "grayscale(100%) brightness(40%) blur(2px)",
             filter: "grayscale(100%) brightness(40%) blur(2px)"
         });
-        $("#" + d + "Tag").css({ height: "100%", "background-color": "rgba(150, 0, 0, 0.15)" });
+        $("#" + d + "Tag").css({ width: "100%", height: "100%", "background-color": "rgba(150, 0, 0, 0.15)" });
         $("#" + d + "Name").text("Defeated");
-        
-        if (gameStatus === 6) {
-            gameOver(false);
-        } else if (gameStatus === 7) {
+
+        if (gameStatus === 7) {
             gameOver(true);
+        } else if (gameStatus === 8) {
+            gameOver(false);
         }
     };
 
@@ -229,26 +233,9 @@ $(document).ready(function () {
         vs.fadeIn(aniSpeed * 2);
     };
 
-    // Attack button pressed
-    atkBtn.on("click", function () {
-        if (gameStatus < 5) {
-            defeated(character.id[defender]);
-        } else {
-            defeated(character.id[attacker]);
-        }
-        gameStatus++;
-        if (gameStatus === 3) {
-            message.text("SELECT YOUR NEXT OPPONENT");
-            message.css({ "font-size": 24, top: (winHeight + 480), left: (winWidth + 310) });
-            message.fadeIn(aniSpeed);
-        } else if (gameStatus === 5) {
-            thirdEnemy();
-        };
-    });
-
     // Game Over
     function gameOver(userWin) {
-        $(".overlay").css( "height", "0" );
+        $(".overlay").css("height", "0");
         notice.css({
             "font-size": 100,
             "font-family": "'Audiowide', cursive",
@@ -262,14 +249,14 @@ $(document).ready(function () {
             notice.css("left", winWidth + 120);
         } else {
             notice.text("You lost...");
-            notice.css("left", winWidth - 320);
+            notice.css("left", winWidth - 330);
         }
 
-        message.text("CLICK HERE TO PLAY AGAIN");
-        message.css({ "font-size": 30, top: (winHeight + 500), left: (winWidth - 75) });
+        message.text("CLICK HERE TO PLAY AGAIN").css("cursor", "pointer");
+        message.css({ "font-size": 30, top: (winHeight + 500), left: (winWidth - 50) });
         message.fadeIn(aniSpeed);
         notice.delay(aniSpeed).fadeIn(aniSpeed);
-        gameStatus = -1;
+        gameStatus = 0;
     };
 
     // Restart the game
@@ -278,16 +265,69 @@ $(document).ready(function () {
         enemyList = ["", "", ""];
         winHeight = ($(window).height() - 450) / 2 + $(window).scrollTop();
         winWidth = ($(window).width() - 300) / 2 + $(window).scrollLeft();
+        clicked = false;
         atkBtn.css({ top: winHeight + 470, left: winWidth - 200 });
         vs.css({ top: winHeight + 185, left: winWidth + 80 });
         gameStatus = 1;
         for (var i = 0; i < character.id.length; i++) {
             var c = character.id[i];
-            $("#" + c).css({ width: "", top: winHeight, left: (winWidth + i * 300 - 450) });
-            $("#" + c + "Tag").css({ height: "", "background-color": character.clr[i] });
+            character.hp[i] = Number($("#" + c + "Pic").attr("hp"));
+            character.atk[i] = Number($("#" + c + "Pic").attr("atk"));
+            character.ct[i] = Number($("#" + c + "Pic").attr("ct"));
+            $("#" + c).css({ "cursor": "pointer", width: "", top: winHeight, left: (winWidth + i * 300 - 450) });
+            $("#" + c + "Tag").css({ width: "", height: "", "background-color": character.clr[i] });
             $("#" + c + "Pic").css({ "-webkit-filter": "", filter: "" });
             $("#" + c + "Name").text(c);
             $("#" + c).hide();
         }
     };
+
+    // Attack button pressed
+    atkBtn.on("click", function () {
+        if (!clicked){
+            var a = character.id[attacker];
+            var a_hp = character.hp[attacker];
+            var a_pow = character.atk[attacker];
+            var d = character.id[defender];
+            var d_hp = character.hp[defender];
+            var d_pow = character.ct[defender];
+
+            d_hp -= a_pow;
+            a_pow += Number($("#" + a + "Pic").attr("atk"));
+
+            $("#" + d + "Tag").css("width", (d_hp / Number($("#" + d + "Pic").attr("hp"))*100) +"%" );
+            $("#" + d + "Name").text("HP " + d_hp);
+
+            if (d_hp > 0) {         // Game status
+                a_hp -= d_pow;      // 0 - Waiting to start the game
+            } else {                // 1 - Selecting main character and 1st opponent
+                gameStatus++;       // 2 - 1st fighting
+                defeated(d);        // 3 - Selecting 2nd opponent
+            }                       // 4 - 2nd fighting
+            if (a_hp <= 0) {        // 5 - Auto selecting last opponent
+                gameStatus = 8;     // 6 - Last fighting
+                defeated(a);        // 7 - User win
+            }                       // 8 - User lose
+
+            $("#" + a + "Tag").css("width", (a_hp / Number($("#" + a + "Pic").attr("hp"))*100) +"%" );
+            $("#" + a + "Name").text("HP " + a_hp);
+
+            character.hp[attacker] = a_hp;
+            character.atk[attacker] = a_pow;
+            character.hp[defender] = d_hp;
+            character.ct[defender] = d_pow;
+
+            if (gameStatus === 3) {
+                message.text("SELECT YOUR NEXT OPPONENT");
+                message.css({ "font-size": 24, top: (winHeight + 480), left: (winWidth + 310) });
+                message.fadeIn(aniSpeed);
+            } else if (gameStatus === 5) {
+                thirdEnemy();
+            };
+
+            // Prevent fast-clicking-bug
+            clicked = true;
+            setTimeout(function(){ clicked = false; }, 500); 
+        };
+    });
 });
